32a9e9d408d3df355bfb649be173bdb14628d66c
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-01-01 16:33:55 -0800

    changes

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,15 +1,55 @@
 // Portfolio page
 
+// Icons
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { IoDocumentTextOutline } from 'react-icons/io5'
+
+// TODO: Split up this entire thing into components
+// TODO: Remove borders
 export default function Home() {
   return (
     <main className="w-full min-h-screen">
       {/* Grouped Header and Sections */}
       <section aria-labelledby="main-content">
         {/* Hero Section */}
-        <header className="w-[60%] min-h-screen border-red-500 border">
-          <p>Hey, I&#39;m</p>
-          <h1>Justin Abuyuan</h1>
-          <p>Engineering Student @ The University of Waterloo</p>
+        {/* TODO: Add fade in transition for this section */}
+        <header className="flex flex-row min-h-screen border-red-500 border">
+          {/* Name */}
+          <section className="w-[60%] pl-28 flex flex-col justify-center min-h-screen border-green-400 border">
+            <section className="w-fit mb-2 border">
+              <p>Hey! I&#39;m</p>
+              <h1 className="text-6xl">Justin Abuyuan</h1>
+            </section>
+
+            {/* Current position */}
+            <section className="w-fit border">
+              <h2 className="text-xl">An engineering student at the University of Waterloo.</h2>
+            </section>
+
+            {/* Lil blurb */}
+            <section className="w-fit mt-5 border">
+              {/* TODO: Implement changing typed text */}
+              I like to code stuff and build things for the internet.
+            </section>
+
+            {/* Social links */}
+            <section className="flex flex-row gap-3 w-fit mt-8 border">
+              {/* TODO: Turn these into buttons and add a cool parallax animation when moving mouse inside the container */}
+              <FaLinkedin className="size-8" />
+              <FaGithub className="size-8" />
+              <IoDocumentTextOutline className="size-8" />
+            </section>
+          </section>
+
+          {/* Hero animation */}
+          {/* TODO: Node animation here */}
+          <section className="pl-20 flex-grow flex flex-col justify-center border border-yellow-500">
+            <section className="w-fit text-xl border">
+              <p>Projects</p>
+              <p>Experience</p>
+              <p>Contact</p>
+            </section>
+          </section>
         </header>
 
         {/*  /!* About Section *!/*/}
@@ -29,20 +69,20 @@ export default function Home() {
         {/*    <h2 id="experience-heading">Experience</h2>*/}
         {/*    <p>Ut vitae varius nisi. Quisque mollis pretium felis, et dictum augue maximus eu.</p>*/}
         {/*  </section>*/}
-        </section>
+      </section>
 
-        {/* Footer */}
-        {/*<footer>*/}
-        {/*  <section id="contact" aria-labelledby="contact-heading">*/}
-        {/*    <h2 id="contact-heading">Get in Touch</h2>*/}
-        {/*    <p>Ut ornare id nisi nec tincidunt. Sed fermentum varius magna et maximus. Cras at lacinia velit, nec*/}
-        {/*      interdum*/}
-        {/*      justo.</p>*/}
-        {/*  </section>*/}
-        {/*  <div>*/}
-        {/*    <p>Footer text</p>*/}
-        {/*  </div>*/}
-        {/*</footer>*/}
+      {/* Footer */}
+      {/*<footer>*/}
+      {/*  <section id="contact" aria-labelledby="contact-heading">*/}
+      {/*    <h2 id="contact-heading">Get in Touch</h2>*/}
+      {/*    <p>Ut ornare id nisi nec tincidunt. Sed fermentum varius magna et maximus. Cras at lacinia velit, nec*/}
+      {/*      interdum*/}
+      {/*      justo.</p>*/}
+      {/*  </section>*/}
+      {/*  <div>*/}
+      {/*    <p>Footer text</p>*/}
+      {/*  </div>*/}
+      {/*</footer>*/}
     </main>
-)
+  )
 }
