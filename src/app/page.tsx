539b1de0113c9c0eab6563c896a3ee08052c2933
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-05-25 20:26:50 -0400

    cool text effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@
 
 export default function Home() {
     return (
-        <div className="flex items-center justify-center h-screen px-64 py-20">
+        <div className="flex items-center justify-center h-screen px-64 py-40">
             <main className="h-full w-full">
                 <header>
-                    <h1>justin abuyuan</h1>
-                    <h2>software eng @ uwaterloo</h2>
+                    <div>
+                        <p>hey, i&#39;m</p>
+                        <h1 className="relative font-bold text-6xl leading-20">
+                            <span className="absolute inset-0 blur-xs">justin abuyuan.</span>
+                            <span className="relative z-10">justin abuyuan.</span>
+                        </h1>
+                    </div>
+
+                    <h2 className="text-xl leading-10">software engineering @ uwaterloo</h2>
+                    <p className="text-gray-400">i like to code stuff and build things for the internet.</p>
                 </header>
             </main>
         </div>
